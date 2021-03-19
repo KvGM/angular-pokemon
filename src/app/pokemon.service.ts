0a18e08c5d8603239f3ff23174d455b9bc338432
KvGM <kevgarcia235@gmail.com>
2021-03-19 23:49:14 +0000

    Details for Pokemon

--- src/app/pokemon.service.ts
+++ src/app/pokemon.service.ts
@@ -2,24 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+const apiUrl = 'https://pokeapi.co/api/v2/';
+
 @Injectable({
   providedIn: 'root',
 })
 export class PokemonService {
-  apiUrl: string = 'https://pokeapi.co/api/v2/';
-
   constructor(private httpClient: HttpClient) {}
 
   getPokemonList(n: number): Observable<any> {
-    return this.httpClient.get<any>(
-      this.apiUrl + 'pokemon?limit=20&offset=' + n
-    );
+    return this.httpClient.get<any>(apiUrl + 'pokemon?limit=20&offset=' + n);
   }
 
   getPokemonUrl(url: string) {
     return this.httpClient.get<any>(url);
   }
+
   getPokemonId(n: number) {
-    return this.httpClient.get<any>(`${this.apiUrl}pokemon/${n}`);
+    return this.httpClient.get<any>(`${apiUrl}pokemon/${n}`);
   }
 }
